ed06e20e385f22acec72d4ff86e473ee0b0fc229
Aumy <aumy@outlook.jp>
2022-06-28 00:35:10 +0900

    Add template

--- flake.nix
+++ flake.nix
@@ -5,5 +5,10 @@
     rec {
       overlays.default = (import ./overlay.nix);
       overlay = overlays.default;
+
+      templates.default = {
+        path = builtins.filterSource (path: type: baseNameOf path == "flake.nix") "./example";
+        description = "Build a SATySFi document.";
+      };
     };
 }
